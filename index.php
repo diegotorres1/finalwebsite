9c96b35c0e5ad5b3681a5d4dd53657945db65e15
Diego Torres <dt@apep.uci.edu>
2018-09-06 15:06:02 -0700

    adding the website

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Diego Torres</title>
+  <meta name="author" content="Diego Torres">
+  <meta name="description" content="diego torres is a computer engineer at UCI" />
+  <meta name="keywords" content="diego, torres, computer,programmer" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="styles/main.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <h1>Diego Torres</h1>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="current" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="current"><a href="index.php">Home</a></li>
+          <li><a href="project.html">Projects</a></li>
+          <li><a href="about_me.html">About Me</a></li>
+          <li><a href="contact.html">Contact Info</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="site_content">
+      <div id="sidebar_container">
+        <div class="sidebar">
+        <!-- insert your sidebar items here -->
+        <h3>Update</h3>
+        <h4>Newest Project</h4>
+          <p>
+              Machine Learning being used to detect incoming objects.<br> For more information click<a> here</a>.
+          </p>
+        </div>
+      </div>
+      <div id="content">
+        <!-- insert the page content here -->
+        <h1>Computer Programmer and Engineer</h1>
+        <p>
+          With the skills obtained from the courses I've taken at the University of California Irvine, the research I've done with the
+          A.P.E.P organization, and the programming projects that I've done in my spare time any new bleeding edge technology feels within
+          my reach of learning and mastering.
+
+        </p>
+        <p>
+            My abilities include :
+        </p>
+        <ul>
+          <li>Mathematical Problem Solving <p>Consistent use of various Mathematical approaches to almost all courses taken</p></li>
+          <li>Machine Learning & OpenCV<p>Used in case for object classification, motion tracking, and pattern recognition</p></li>
+          <li>Data Analytics<p>Analytics provided key ways to present information and extrapolate patterns from the data used in my research</p></li>
+          <li>Databases<p>Provides convenient mechanisms for accessing particular information from any location</p></li>
+          <li>Engineering<p>Interacting with the world and understanding it's physical behavior are key to developing interesting practical ideas</p></li>
+        </ul>
+      </div>
+    </div>
+    <div id="footer">
+      <p>email : diegot1@uci.edu  |  phone number : (909)-386-8987  |  LinkedIn : <a href="https://www.linkedin.com/in/diego-torres-557766157">Profile</a></p>
+    </div>
+  </div>
+</body>
+</html>
