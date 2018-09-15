0b356aad4b2df57a25e45a4e983aee36c95bf859
Diego Torres <dt@apep.uci.edu>
2018-09-14 23:52:47 -0700

    adding more content like the gallery and changing the main page to resemble more of a resume

--- index.php
+++ index.php
@@ -41,22 +41,61 @@
       <div id="content">
         <!-- insert the page content here -->
         <h1>Computer Programmer and Engineer</h1>
-        <p>
-          With the skills obtained from the courses I've taken at the University of California Irvine, the research I've done with the
-          A.P.E.P organization, and the programming projects that I've done in my spare time any new bleeding edge technology feels within
-          my reach of learning and mastering.
+        <div>
+          <p>
+            <h4>Education</h4>
+            <h2>University of California, Irvine</h2>
+            Computer Engineer
+            <br>
+            <u>Bachelors in Science</u>, expected 05/19
+            <br>
+            <strong><u>Projects:</u></strong>
+            <br><br><br>
+            <ol>
+              <li>Programming a Basys FPGA board using System Verilog to simulate a basic processor and its individual components. Worked in a group of four to reproduce the concepts learned from the course, Logic and Digital Systems.</li>
+              <li>Android Platform making retro-games. Collaborated  in a team of four people. Led to further understanding  the limitations of programming due to hardware. Useful for understanding systems in parallel.</li>
+              <li>Current source under Professor Lee of UCI.  Worked with one group member to gather data and present it to other students in a lecture format. Helped constrain focus onto one topic for a more thorough understanding.</li>
+              <li>Taxi  system in C, using server to client communications to send data of available cars in a premade map and communicating a request to the car. Used SDL to implement a GUI, and  a CVS repository to work together on the source code. Worked in a team of four.</li>
+              <li>Self-Adjusting Solar Panel, using Arduino microcontroller to change the angle of the solar panel depending on the light intensity available in the surrounding area.  Used knowledge of SolidWorks, Simple Circuit Analysis, and 3D printing to construct device.</li>
+            </ol>
+            <br><br><br>
+          <p>
+        </div>
+        <div class = "experience">
+          <h4>Experience</h4>
+          <p>
+            <u>Programming Languages</u>: Python, Java, C++, System Verilog, mySQL, OpenGl*(API)
+            <br>
+            <u>Software</u>: Xilinx ISE, mySQL, Android Studio, Blender, SolidWorks, Analog Discovery Waveforms,QuestaSim
+            <br>
+            <u>Hardware</u>: General Purpose Measuring Equipment For Electronics, Analog Discovery 2, Basys 3 Artix-7 FBGA , Printrbot Simple
+          </p>
+          <p>
+            <strong><u>Individual Study: Personal Electronics</u></strong><i> Sep 2017 -Mar 2018</i>
+            <br>
+            <i>Coordinator: Professor H. P. Lee of UCI : email - hplee3@yahoo.com</i>
+            <br>
+            Further my understanding of the fundamentals of circuits by analyzing more vigorous and challenging applications of components learned in corresponding electronics courses. Use of circuit analysis and general physics understanding for every topic covered.
 
-        </p>
-        <p>
-            My abilities include :
-        </p>
-        <ul>
-          <li>Mathematical Problem Solving <p>Consistent use of various Mathematical approaches to almost all courses taken</p></li>
-          <li>Machine Learning & OpenCV<p>Used in case for object classification, motion tracking, and pattern recognition</p></li>
-          <li>Data Analytics<p>Analytics provided key ways to present information and extrapolate patterns from the data used in my research</p></li>
-          <li>Databases<p>Provides convenient mechanisms for accessing particular information from any location</p></li>
-          <li>Engineering<p>Interacting with the world and understanding it's physical behavior are key to developing interesting practical ideas</p></li>
-        </ul>
+            <ol>
+              <li>position of managing presentations for Texas Instruments Sensor topic, completing TI  RSLK beta run</li>
+              <li>presenting personal topics every week and providing individual insight into findings</li>
+            </ol>
+          </p>
+          <p>
+            <strong><u>Undergraduate Researcher</u></strong> <i>Feb 2018 ~ Advanced Power and Energy Program, University of California Irvine</i>
+            <br>
+            <i>Mentor Graduate Student: James Soukup : email - jvs@apep.uci.edu</i>
+            <ol>
+              <li>Developing a system for pulling data from a MySQL database that would be used during normal operation for their MatLab program, HiGrid. Currently looking through all files in HiGrid  for potential uses when designing Python API for the database.</li>
+              <li>Hydrogen station vehicle analysis for design consultation</li>
+              <li>Powertrain analysis for consumer performance vs efficiency correlation</li>
+            </ol>
+          </p>
+
+
+
+        </div>
       </div>
     </div>
     <div id="footer">
